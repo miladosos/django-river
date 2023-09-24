ee4f7f5915a3e8b0cddf6ac77568f547ff784c7e
Milad Noroozi <norooziosos@gmail.com>
2023-09-24 15:26:13 +0330

    [FIX] Requirements

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
     long_description=long_description,
     install_requires=[
         "Django",
-        "django-mptt==0.9.1",
-        "django-cte==1.1.4",
+        "django-mptt==0.14.0",
+        "django-cte==1.2.1",
         "django-codemirror2==0.2"
     ],
     include_package_data=True,
